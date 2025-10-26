ca9b01fe4f8bc627c587972b3102bccbaf32a9d2
DemiGod <demigod14100@gmail.com>
2025-10-26 13:48:31 +0530

    Add Navbar component to Home page for improved navigation

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import HomeSwitcher from "./home-switcher";
 import AlternativeHomePage from '../components/AlternativeHomePage';
 import HeroSection from "@/components/herosection";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <HeroSection />
       <AlternativeHomePage />
     </>
