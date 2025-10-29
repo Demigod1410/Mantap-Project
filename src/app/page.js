c6f162e99832b6d31b31a79886d820b82baa95ca
DemiGod <demigod14100@gmail.com>
2025-10-29 16:12:54 +0530

    Refactor Home component to replace AlternativeHomePage with HomePage and reorganize imports for better structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,22 @@
-import HomeSwitcher from "./home-switcher";
-import AlternativeHomePage from '../components/AlternativeHomePage';
+
 import HeroSection from "@/components/herosection";
 import Navbar from "@/components/navbar";
+import HomePage from "@/components/HomePage";
+import FeaturesPage from '@/components/featurespage';
+import AboutUs from '@/components/aboutus';
+import WhyUs from '@/components/whyus';
+import Footer from '@/components/footer';
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <HeroSection />
-      <AlternativeHomePage />
+      <HomePage />
+      <FeaturesPage />
+      <AboutUs />
+      <WhyUs />
+      <Footer />
     </>
   );
 }
